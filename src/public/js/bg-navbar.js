74b541bf418a126a710e8acdfb8f56385bf8516d
leon-rdo <leonardoabreu1212@gmail.com>
2023-02-28 17:05:42 -0300

    Background changed
    A dark background is aplyed to spotlight the elements of the website.

--- src/public/js/bg-navbar.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 50) {
-    nav.classList.add("bg-dark", "shadow");
-  } else {
-    nav.classList.remove("bg-dark", "shadow");
-  }
-});
